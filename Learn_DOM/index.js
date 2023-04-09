07e36f90aa55dbe279b7957ccf3570ac46a10e3e
Mon <moniquenagel24.mn@gmail.com>
2023-04-09 11:34:51 +0200

    Finished first exercise.

--- Learn_DOM/index.js
+++ Learn_DOM/index.js
@@ -1,6 +1,7 @@
-function buttonClick(){
+// EXERCISE 1
+function js_style(){
 
-    alert 
-
-
+    document.getElementById("text").style.color="red"
+    document.getElementById("text").style.fontWeight="bold"
+    document.getElementById("text").style.fontSize="large"
 }
