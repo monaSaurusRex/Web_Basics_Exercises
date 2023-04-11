cbd4343304727668e40b81fc45388b1e8105cf70
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-11 10:41:37 +0200

    Added files for activity to create basic html for and capturing data and printing it out in the console.

---
+++ Basic_HTML_Form/index.js
@@ -0,0 +1,23 @@
+function getUserData(){
+
+
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+
+
+    console.log(`
+        First Name: ${fname}
+         Last Name: ${lname}
+             Email: ${email}
+          Province: ${province} 
+    
+    
+    `)
+
+    console.log(lname)
+
+
+
+    
+}
