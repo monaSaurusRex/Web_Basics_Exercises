d61582781b916c1044a905782a0deaacbc9a64d6
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-14 11:34:49 +0200

    Shortened fizzBuzz code by making use of ternary operator.

--- Typescript_Basics/fizzBuzz.ts
+++ Typescript_Basics/fizzBuzz.ts
@@ -11,19 +11,32 @@ Otherwise, just say the number as you would normally
 
 function fizzBuzz(num: number){
 
-    switch(true){
-        case (num%3===0 && num%5===0):
-            return "Fizz-Buzz!"
-        case (num%3==0):
-            return "Fizz!"     
-        case(num%5==0):
-            return "Buzz!";
-        default:
-            return num
-    }
+    // switch(true){
+    //     case (num%3===0 && num%5===0):
+    //         return "Fizz-Buzz!"
+    //     case (num%3==0):
+    //         return "Fizz!"     
+    //     case(num%5==0):
+    //         return "Buzz!";
+    //     default:
+    //         return num
+    // }
+
+    //ternary operator 
+    (num%3===0 && num%5===0)? console.log("Fizz-Buzz!") : 
+    (num%3==0) ? console.log("Fizz!") : 
+    (num%5==0) ? console.log("Buzz!" ) :
+    console.log(num)
+
 }
 
-console.log(fizzBuzz(15))
-console.log(fizzBuzz(3))
-console.log(fizzBuzz(5))
-console.log(fizzBuzz(2))
+// console.log(fizzBuzz(15))
+// console.log(fizzBuzz(3))
+// console.log(fizzBuzz(5))
+// console.log(fizzBuzz(2))
+
+//ternary operator
+fizzBuzz(15)
+fizzBuzz(3)
+fizzBuzz(5)
+fizzBuzz(2)
